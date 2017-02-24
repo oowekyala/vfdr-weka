85e494c42b6977f76ef4fd3d563517b85814d7b5
Clément Fournier <clement.fournier@insa-rennes.fr>
2017-02-24 12:21:04 +0100

    Added comments

--- Vfdr-weka/src/vfdr/EntropyMetric.java
+++ Vfdr-weka/src/vfdr/EntropyMetric.java
@@ -13,6 +13,22 @@ import weka.core.ContingencyTables;
  */
 public class EntropyMetric extends ExpansionMetric {
 
+	/**
+	 * Evaluates expansion candidates based on the post-expansion class
+	 * distribution predicted for the rule. If several post-expansion
+	 * distributions are found, their score is put in order in the array
+	 * returned.
+	 * 
+	 * When evaluating an expansion on a numeric attribute, once the splitpoint
+	 * is chosen, the antecedent could take either a <= or > condition. Both
+	 * distributions are evaluated so as to chose the best condition
+	 * 
+	 * @param preDist
+	 *            Previous distribution
+	 * @param postDists
+	 *            Post-expansion distributions
+	 * @return An array of one score for each post-expansion distribution
+	 */
 	@Override
 	public double[] evaluateSplit(Map<String, Integer> preDist, List<Map<String, Integer>> postDist) {
 
--- Vfdr-weka/src/vfdr/ExpansionMetric.java
+++ Vfdr-weka/src/vfdr/ExpansionMetric.java
@@ -3,8 +3,30 @@ package vfdr;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Frames objects able to evaluate the merit of an expansion to chose the best.
+ * 
+ * @author Clément Fournier (clement.fournier@insa-rennes.fr)
+ *
+ */
 public abstract class ExpansionMetric {
 
-	public abstract double[] evaluateSplit(Map<String, Integer> preDist, List<Map<String, Integer>> postDist);
+	/**
+	 * Evaluates expansion candidates based on the post-expansion class
+	 * distribution predicted for the rule. If several post-expansion
+	 * distributions are found, their score is put in order in the array
+	 * returned.
+	 * 
+	 * When evaluating an expansion on a numeric attribute, once the splitpoint
+	 * is chosen, the antecedent could take either a <= or > condition. Both
+	 * distributions are evaluated so as to chose the best condition
+	 * 
+	 * @param preDist
+	 *            Previous distribution
+	 * @param postDists
+	 *            Post-expansion distributions
+	 * @return An array of one score for each post-expansion distribution
+	 */
+	public abstract double[] evaluateSplit(Map<String, Integer> preDist, List<Map<String, Integer>> postDists);
 
 }

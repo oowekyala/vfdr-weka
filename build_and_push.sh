0b1b105b95ae0a0fa9134812f6cab5333f381144
oowekyala <clement.fournier76@gmail.com>
2017-05-28 18:35:45 +0200

    ./build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/sh
+#!/bin/bash
 ant make_package -Dpackage=vfdr-1.0.0 -buildfile build_package.xml > /dev/null
 
-git add * 
-git commit -qm "$0"
+git commit -aqm "$0"
 
 git push origin :refs/tags/v1.0.0 -q
 git tag -qfa v1.0.0

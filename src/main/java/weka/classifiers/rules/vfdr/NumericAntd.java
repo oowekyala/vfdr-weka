0414a22ce3a8c02e15a01aeebdba33c8a0fd7f63
oowekyala <clement.fournier76@gmail.com>
2017-05-30 23:50:48 +0200

    Cosmetic modifications

--- src/main/java/weka/classifiers/rules/vfdr/NumericAntd.java
+++ src/main/java/weka/classifiers/rules/vfdr/NumericAntd.java
@@ -12,38 +12,64 @@ import weka.core.Instance;
  * @version VFDR-Base
  */
 public class NumericAntd extends Antd {
-    
+
     /** For serialisation */
     private static final long serialVersionUID = 8837859958896793453L;
-    
+
     /** The split point for this numeric antecedent */
-    private double            m_splitPoint;
-    
+    private double m_splitPoint;
+
     /** Is 0 if the condition is &lt;=, 1 otherwise */
-    private boolean           m_conditionHigher;
-    
+    private boolean m_conditionHigher;
+
     /**
      * Builds a numeric antecedent from the attname
      *
-     * @param attribute
-     *            The attribute's name
+     * @param attribute The attribute's name
      */
     public NumericAntd(Attribute attribute) {
         m_attribute = attribute;
         m_splitPoint = Double.NaN;
         m_isNominal = false;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        NumericAntd that = (NumericAntd) o;
+
+        if (Double.compare(that.m_splitPoint, m_splitPoint) != 0) {
+            return false;
+        }
+        return m_conditionHigher == that.m_conditionHigher;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(m_splitPoint);
+        result = (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (m_conditionHigher ? 1 : 0);
+        return result;
+    }
+
     @Override
     public boolean covers(Instance inst) {
         return m_conditionHigher ? inst.value(m_attribute) > m_splitPoint : inst.value(m_attribute) <= m_splitPoint;
     }
-    
+
     @Override
     public String toString() {
         return m_attribute.name() + (m_conditionHigher ? " > " : " <= ") + Math.floor(m_splitPoint * 1000) / 1000;
     }
-    
+
     /**
      * Get split point of this numeric antecedent
      *
@@ -52,17 +78,17 @@ public class NumericAntd extends Antd {
     public double getSplitPoint() {
         return m_splitPoint;
     }
-    
+
     public void setSplitPoint(double m_splitPoint) {
         this.m_splitPoint = m_splitPoint;
     }
-    
+
     public boolean isConditionHigher() {
         return m_conditionHigher;
     }
-    
+
     public void setConditionHigher(boolean m_condition) {
         m_conditionHigher = m_condition;
     }
-    
+
 }
--- src/main/java/weka/classifiers/rules/vfdr/VfdrRule.java
+++ src/main/java/weka/classifiers/rules/vfdr/VfdrRule.java
@@ -3,8 +3,10 @@ package weka.classifiers.rules.vfdr;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import weka.classifiers.rules.Vfdr;
 import weka.core.Instance;
@@ -18,37 +20,35 @@ import weka.core.Utils;
  *
  * @author Clément Fournier (clement.fournier@insa-rennes.fr)
  * @version VFDR-Base
- *
  * @see SufficientStats
  */
 public class VfdrRule implements Serializable {
-    
+
     /** For serialisation */
     private static final long serialVersionUID = -388653429728539867L;
-    
+
     /** The literals (antecedents) that make up this rule */
-    private List<Antd>        m_literals;
-    
+    private List<Antd> m_literals;
+
     /** Sufficient statistics */
-    private SufficientStats   m_lr;
-    
+    private SufficientStats m_lr;
+
     /** Callback to the classifier */
-    private Vfdr              m_classifierCallback;
-    
+    private Vfdr m_classifierCallback;
+
     /**
      * Builds a VfdrRule
      *
-     * @param vfdr
-     *            The classifier that owns this object
+     * @param vfdr The classifier that owns this object
      */
     public VfdrRule(Vfdr vfdr) {
         m_classifierCallback = vfdr;
         m_literals = new ArrayList<>();
         m_lr = m_classifierCallback.getUseNaiveBayes() ? new SufficientStats.NaiveBayes(m_classifierCallback)
-                : new SufficientStats.MajorityClass(m_classifierCallback);
-        
+            : new SufficientStats.MajorityClass(m_classifierCallback);
+
     }
-    
+
     /**
      * Gets the sufficient statistics of the rule
      *
@@ -130,7 +130,7 @@ public class VfdrRule implements Serializable {
         }
         return true;
     }
-    
+
     /**
      * Whether this rule has antecedents, i.e. whether it is a default rule or
      * not
@@ -140,7 +140,7 @@ public class VfdrRule implements Serializable {
     public boolean hasAntds() {
         return m_literals.size() > 0;
     }
-    
+
     /**
      * The size of the rule, i.e. number of antecedents
      *
@@ -149,39 +149,73 @@ public class VfdrRule implements Serializable {
     public double size() {
         return m_literals.size();
     }
-    
+
     /**
      * Computes the Hoeffding bound for the given parameters
      *
-     * @param range
-     *            Range of the split metric
-     * @param confidence
-     *            Confidence threshold
-     * @param weight
-     *            Weight of the observations made so far with this rule
+     * @param range      Range of the split metric
+     * @param confidence Confidence threshold
+     * @param weight     Weight of the observations made so far with this rule
+     *
      * @return Hoeffding bound
      */
     public double computeHoeffding(double range, double confidence, double weight) {
         return Math.sqrt(((range * range) * Math.log(1.0 / confidence)) / (2.0 * weight * Utils.log2));
     }
-    
-    /**
-     * Returns a descriptive string
-     *
-     * @return A descriptive string
-     */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        VfdrRule vfdrRule = (VfdrRule) o;
+
+        return m_literals.equals(vfdrRule.m_literals);
+    }
+
+    @Override
+    public int hashCode() {
+        return m_literals.hashCode();
+    }
+
+
     @Override
     public String toString() {
-        String s = "{" + ((m_literals.size() > 0) ? m_literals.get(0) : "");
-        for (int i = 1; i < m_literals.size(); i++) {
-            s += " and " + m_literals.get(i).toString();
+        String s = "";
+        if (m_literals.size() > 0) {
+            s = "(" + m_literals.get(0).toString() + ")";
+            for (int i = 1; i < m_literals.size(); i++) {
+                s += " and (" + m_literals.get(i).toString() + ")";
+            }
+            s += " ";
+        } else {
+            s = "()";
         }
-        s += "} -> ";
-        
+
+        s += " -> ";
+
+        List<Map.Entry<String, Integer>> sortedDist = new ArrayList<>();
+
         for (Map.Entry<String, Integer> e : m_lr.m_classDistribution.entrySet()) {
+            sortedDist.add(e);
+        }
+
+        Collections.sort(sortedDist, new Comparator<Entry<String, Integer>>() {
+                @Override
+                public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+                    return Double.compare(o2.getValue(), o1.getValue());
+                }
+            }
+        );
+
+        for (Map.Entry<String, Integer> e : sortedDist) {
             s += e.getKey() + " (" + Math.floor(1000 * e.getValue().doubleValue() / m_lr.m_totalWeight) / 1000 + "), ";
         }
-        
+
         return s + "\t (total weight: " + m_lr.m_totalWeight + ")";
     }
 }
